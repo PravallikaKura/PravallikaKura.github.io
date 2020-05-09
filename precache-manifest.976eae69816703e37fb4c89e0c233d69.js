9e9a381837efb203ae8bd2b96b0fcd9900474b7e
PravallikaKura <pravallikakura@gmail.com>
2020-05-09 18:48:20 +0530

    Updates

---
+++ precache-manifest.976eae69816703e37fb4c89e0c233d69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9ba1f0bb81b5a5172b1351522e1b02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "420187c358987216f8de",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "600bc80ab5444b183787",
+    "url": "/static/js/2.2a4ac1d7.chunk.js"
+  },
+  {
+    "revision": "420187c358987216f8de",
+    "url": "/static/js/main.8d37e69a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d292e21532ea5857531cdb8b0180637",
+    "url": "/static/media/profile.5d292e21.jpg"
+  }
+]);
